3c4386a44aec3d7c52d140f539aab72cd5746dfc
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-11-14 11:22:02 -0300

    First styling on buttons

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,14 +1,29 @@
 import PrimaryButton from '@/components/PrimaryButton';
-import { TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25
+  }
+});
 
 export default StartGameScreen;
