29f7804ad8f69455c6e1cc8cb83e698261cc3d39
Carlos Adolfo <adolfo.ggc@gmail.com>
2026-01-08 17:21:17 -0300

    Add Status Bar

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ import StartGameScreen from "@/screens/StartGameScreen";
 import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { ImageBackground, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -65,21 +66,24 @@ export default function Index() {
   }
 
   return (
-    <LinearGradient 
-      colors={[Colors.primary700, Colors.accent500]} 
-      style={styles.rootScreen}>
-      <ImageBackground 
-        source={require('../assets/images/background.png')}
-        resizeMode='cover'
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
+    <>
+      <StatusBar style='light'/>
+      <LinearGradient 
+        colors={[Colors.primary700, Colors.accent500]} 
+        style={styles.rootScreen}>
+        <ImageBackground 
+          source={require('../assets/images/background.png')}
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
 
-      >
-        <SafeAreaView style={styles.rootScreen}>
-          { screen }
-        </SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        >
+          <SafeAreaView style={styles.rootScreen}>
+            { screen }
+          </SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
